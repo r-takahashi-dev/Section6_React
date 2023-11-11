69767bde44648e7786c825bbd610181fb61649f2
r-takahashi <147617856+r-takahashi-dev@users.noreply.github.com>
2023-11-11 14:12:20 +0000

    Update App.jsx and colorfulMessage.jsx Section6_48

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,41 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import ColorfulMessage from './components/colorfulMessage';
 
 const App = () => {
-    const onClickCountUp = () => {
-      setNum(num + 1);
-    };
-    const [num, setNum] = useState(0);
-    return (
-        <>
-          <h1 style={{ color: 'red' }}>こんにちは</h1>
-          <ColorfulMessage color='blue'>お元気ですか？</ColorfulMessage>
-          <ColorfulMessage color='pink'>元気です！</ColorfulMessage>    
-          <button onClick={onClickCountUp}>カウントアップ</button>
-          <p>{num}</p>
-        </>
-    );
+  console.log("さいしょ");
+  const [num, setNum] = useState(0);
+  const [faceShowFlg, setFaceShowFlg] = useState(false);
+
+  const onClickCountUp = () => {
+    setNum(num + 1);
+  };
+  const onClickSwitchShowlg = () => {
+    setFaceShowFlg(!faceShowFlg);
+  };
+
+  useEffect(() => {
+    if (num > 0) {
+      if (num % 3 === 0) {
+        faceShowFlg || setFaceShowFlg(true);
+      } else {
+        faceShowFlg && setFaceShowFlg(false);
+      }
+    }
+  
+  }, [num]);
+
+  return (
+      <>
+        <h1 style={{ color: 'red' }}>こんにちは</h1>
+        <ColorfulMessage color='blue'>お元気ですか？</ColorfulMessage>
+        <ColorfulMessage color='pink'>元気です！</ColorfulMessage>    
+        <button onClick={onClickCountUp}>カウントアップ</button>
+        <br />
+        <button onClick={onClickSwitchShowlg}>on/off</button>
+        <p>{num}</p>
+        {faceShowFlg && <p>( ﾟДﾟ)</p>}
+      </>
+  );
 };
 
 export default App;
--- src/components/colorfulMessage.jsx
+++ src/components/colorfulMessage.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 
 const colorfulMessage = (props) => {
-    const {color, children} = props;
-    const contentStyle = {
-      color,
-      fontSize: '18px'
-    };
-    return (
-        <p style={contentStyle}>{children}</p>
-    );
+  const {color, children} = props;
+  const contentStyle = {
+    color,
+    fontSize: '18px'
+  };
+  return (
+      <p style={contentStyle}>{children}</p>
+  );
 };
 
 export default colorfulMessage;
